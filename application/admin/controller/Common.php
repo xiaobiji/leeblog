14887287bce4731ae823ff84e90bd3715c54db1f
AbnerLYZ <Abner3721@163.com>
2019-07-04 18:01:45 +0800

    添加点赞功能（一半未完成）

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -108,4 +108,19 @@ class Common extends Controller
         ];
         return $data;
     }
+    public function getIp(){
+        //ip是否来自共享互联网
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+        }
+//ip是否来自代理
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+//ip是否来自远程地址
+        else {
+            $ip_address = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip_address;
+    }
 }
